6c6ab045022b6f34420a87fc2fdc0fe62621c7d5
Vasilis Chaniotis <bchaniotis@hotmail.com>
2019-07-02 03:04:30 +0300

    After lecture 53

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,12 @@
 <template>
     <ul>
-        VideoList
-        {{ videos.length }}
+        <VideoListItem v-for="video in videos">
+        </VideoListItem>
     </ul>
 </template>
 
 <script>
-import { VideoListItem } from './VideoListItem';
+import VideoListItem from './VideoListItem';
 
 export default {
     name: 'VideoList',
